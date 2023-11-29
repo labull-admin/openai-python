4621fd8558c832567544307d7903d2c3d9205e4f
softpug1023 <120373951+softpug1023@users.noreply.github.com>
2023-11-29 16:54:34 +0800

    tunnel check every 10 seconds, restart tunel if inactive

--- src/openai/labull_proxy.py
+++ src/openai/labull_proxy.py
@@ -1,7 +1,6 @@
 
-
-from typing import Union, Dict
-import httpx
+import asyncio
+import threading
 import os
 from sshtunnel import SSHTunnelForwarder
 
@@ -14,6 +13,8 @@ SSH_KEY_PATH = os.getenv('PROXY_LABULL_ORG_SSH_KEY_PATH')
 REMOTE_HOST = 'proxy.labull.org'
 REMOTE_PORT = 2046  # Port of the remote proxy server
 
+print(
+    f"Connecting to {SSH_SERVER}:{SSH_PORT} with username {SSH_USERNAME} and key {SSH_KEY_PATH}")
 # Establish an SSH tunnel
 tunnel = SSHTunnelForwarder(
     (SSH_SERVER, SSH_PORT),
@@ -23,17 +24,49 @@ tunnel = SSHTunnelForwarder(
     local_bind_address=('127.0.0.1', 0)  # Set to '0' to find an available port
 )
 
-# Start the tunnel
-tunnel.start()
+# Function to start the tunnel
 
-print(f"Tunnel is active on localhost:{tunnel.local_bind_port}, "
-      f"and it is forwarding to {REMOTE_HOST}:{REMOTE_PORT}")
+
+def start_tunnel():
+    tunnel.start()
+    print(f"Labull Proxy Tunnel is active on localhost:{tunnel.local_bind_port}, "
+          f"and it is forwarding to {REMOTE_HOST}:{REMOTE_PORT}")
+
+# Function to stop the tunnel
+
+
+def stop_tunnel():
+    if tunnel.is_active:
+        tunnel.stop()
+        print("Labull Proxy Tunnel stopped")
+
+
+# Asynchronous function to check and restart the tunnel
+async def check_tunnel():
+    while True:
+        if not tunnel.is_active:
+            print("Labull Proxy Tunnel is not active, trying to start...")
+            start_tunnel()
+        await asyncio.sleep(10)  # Check every 30 seconds
+
+# Function to run the asynchronous loop in a separate thread
+
+
+def run_async_loop(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(check_tunnel())
+
+
+# start_tunnel
+start_tunnel()
+# Create a new event loop for the background thread
+new_loop = asyncio.new_event_loop()
+t = threading.Thread(target=run_async_loop, args=(new_loop,))
+t.start()
 
 # Now you can use the tunnel with a proxy-aware HTTP client or library.
 
-proxies= {
+proxies = {
     'http://': f'http://localhost:{tunnel.local_bind_port}',
     'https://': f'http://localhost:{tunnel.local_bind_port}'
 }
-
-
